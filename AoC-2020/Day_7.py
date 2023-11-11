cff46fab7f97c385c7f6990b9dd8f3f88fb3b978
Tom Brooks <tbrooks2706@gmail.com>
2023-11-11 20:15:06 +0000

    days 7-9 prep

---
+++ AoC-2020/Day_7.py
@@ -0,0 +1,14 @@
+import general_functions
+from copy import deepcopy
+
+#https://adventofcode.com/2020/day/7
+
+#summary and instructions
+
+init_list = read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC\AoC-2020\Day_7_test.txt")
+
+# part_1_answer = pass
+# print(part_1_answer)
+
+# part_2_answer = pass
+# print(part_2_answer)
