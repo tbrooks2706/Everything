a52cb8e5bc8617c629af576bbcc1aead91763783
Tom Brooks <tbrooks2706@gmail.com>
2023-12-01 21:44:36 +0000

    day 1 complete

--- AoC-2023/Day_1.py
+++ AoC-2023/Day_1.py
@@ -9,26 +9,27 @@ import copy
 #if there's only one number, repeat it (abc1de -> 11)
 #sum all 2 digit numbers from all strings
 
-init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC\AoC-2023\Day_1_test_pt2.txt")
-print(init_list)
-
-number_dict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
-
 class CalibrationLine:
     def __init__(self, string) -> None:
         self.full_string = string
         self.edited_string = self.replace_words_with_numbers(self.full_string)
-        self.letters = self.separate_string(self.edited_string)[0]
-        self.numbers = self.separate_string(self.edited_string)[1]
-        self.value = int(self.numbers[0] + self.numbers[-1])
+        self.letters1 = self.separate_string(self.full_string)[0]
+        self.numbers1 = self.separate_string(self.full_string)[1]
+        try:
+            self.value1 = int(self.numbers1[0] + self.numbers1[-1])
+        except:
+            self.value1 = 0
+        self.letters2 = self.separate_string(self.edited_string)[0]
+        self.numbers2 = self.separate_string(self.edited_string)[1]
+        try:
+            self.value2 = int(self.numbers2[0] + self.numbers2[-1])
+        except:
+            self.value2 = 0
 
-    #######need to change this function so it runs through the string left to right, instead of looking for the numbers in number order#######
-    ######"zoneight" changes to "z1ight", "eightwothree" -> "8wo3", "xtwone3" -> "x2ne3" (it picks up the "two" not the "one") ########
-    
     def replace_words_with_numbers(self, input_string):
         new_string = copy.deepcopy(input_string)
         for key, value in number_dict.items():
-            new_string = new_string.replace(key, value)
+            new_string = new_string.replace(key, key+value+key)
         return new_string
 
     def separate_string(self, input_string):
@@ -44,14 +45,16 @@ class CalibrationLine:
         return [letters, numbers]
 
 def sum_values(input_list):
-    total = 0
+    total1 = 0
+    total2 = 0
     for item in input_list:
         this_line = CalibrationLine(item)
-        total += this_line.value
-    return total
+        total1 += this_line.value1
+        total2 += this_line.value2
+    return [total1, total2]
 
-part_1_answer = sum_values(init_list)
-print(part_1_answer)
+init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC\AoC-2023\Day_1.txt")
+number_dict = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
-# part_2_answer = pass
-# print(part_2_answer)
+answers = sum_values(init_list)
+print(answers)
