cad74a5a6216b09027dc5c4a53e9b3e9d380de54
Tom Brooks <tbrooks2706@gmail.com>
2023-12-03 10:05:15 +0000

    setting up days 3-5

---
+++ AoC-2023/Day_3.py
@@ -0,0 +1,14 @@
+import general_functions
+from copy import deepcopy
+
+#https://adventofcode.com/2023/day/3
+
+#summary and instructions
+
+init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC\AoC-2023\Day_3_test.txt")
+
+# part_1_answer = pass
+# print(part_1_answer)
+
+# part_2_answer = pass
+# print(part_2_answer)
--- AoC-2023/Day_5.py
+++ AoC-2023/Day_5.py
@@ -1,11 +1,11 @@
 import general_functions
 from copy import deepcopy
 
-#https://adventofcode.com/2023/day/8
+#https://adventofcode.com/2023/day/5
 
 #summary and instructions
 
-init_list = read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC\AoC-2023\Day_8_test.txt")
+init_list = general_functions.read_file(r"C:\Users\Tom.Brooks\OneDrive - BJSS Ltd\Documents\Coding\AoC\AoC-2023\Day_4_test.txt")
 
 # part_1_answer = pass
 # print(part_1_answer)
