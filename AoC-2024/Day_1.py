1d9cd7c141086b6426df9b888d743d1dd39069f1
Tom Brooks <tbrooks2706@gmail.com>
2024-12-02 14:13:25 +0000

    day 1 prep

---
+++ AoC-2024/Day_1.py
@@ -0,0 +1,10 @@
+import general_functions
+
+#https://adventofcode.com/2024/day/1
+
+#summary and instructions
+
+
+init_list = general_functions.read_file(r"C:\Users\tbroo\OneDrive\Documents\Code\Everything\AoC-2024\Day_1_test.txt")
+print(init_list)
+
